6283ae24e6b6266fcc83fbeeb9f9052397cccdd0
JavierP08 <A01662438@tec.mx>
2022-11-26 00:40:36 +0000

    Practica 3

--- Server.py
+++ Server.py
@@ -1,20 +1,26 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class MyServer(BaseHTTPRequestHandler):
-    def _set_response(self):
+    def set_response(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header("Content type", "text/html")
         self.end_headers()
-
+    
     def do_GET(self):
-        print("hola desde el get")
-        if "/sensor1_temp" in self.path:
-            sensor1_temp = self.path.split("=")[1]
-            print ("La temperatura es ()".format(sensor1_temp))
-        self._set_response()
-        self.wfile("Hola este es mi super server. GET request for()".format(self.path).encode('utf-8'))
+        print("Hola desde el get")
+        if "/sensor1_distancia" in self.path:
+            sensor1_distancia = self.path.split("=")[1]
+            print("La distancia es {}".format(sensor1_distancia))
+            if "sensor2_humedadTemp" in self.path:
+                sensor2_humedadTemp = self.path.split("=")[1]
+                print("La humedad y la temperatura son {}".format(sensor2_humedadTemp))
+                if "sensor3_nivelLuz" in self.path:
+                    sensor3_nivelLuz = self.path.split("=")[1]
+                    print("El porcentaje de nivel de luz es {}".format(sensor3_nivelLuz))        
+        self.set_response()
+        self.wfile.write("Hola este es mi super server. GET request for {}".format(self.path).encode("utf-8"))
 
-port = 8880
-server_address = ('',port)
+port = 8080
+server_address = ("",port)
 httpd = HTTPServer(server_address, MyServer)
 httpd.serve_forever()
